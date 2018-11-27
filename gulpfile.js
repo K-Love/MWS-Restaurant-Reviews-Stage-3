c6a81aecd9ee9b6bf94b3edd26300e231efd2d93
Kevin <kevin@backstageinteractive.com>
2018-11-26 19:25:42 -0800

    feat: Update Gulp file to serve the app

--- gulpfile.js
+++ gulpfile.js
@@ -24,3 +24,18 @@ gulp.task("styles", function() {
       .pipe(gulp.dest("./css"))
       .pipe(browserSync.stream());
   });
+
+  // Build and serve the app
+gulp.task('serve:dist', ['default'], function () {
+  browserSync.init({
+    server: 'dist',
+    port: 8000
+  });
+
+  gulp.watch(['/*.html'], ['html:dist', reload]);
+  gulp.watch(['css/*.css'], ['html:dist', reload]);
+  gulp.watch(['/js/*.js', '!app/js/dbhelper.js', '!app/js/idbhelper.js'], ['lint', 'html:dist', reload]);
+  gulp.watch(['/sw.js', 'app/js/idbhelper.js'], ['lint', 'sw:dist', reload]);
+  gulp.watch(['/js/dbhelper.js', 'app/js/idbhelper.js'], ['lint', 'dbhelper:dist', 'html:dist', reload]);
+  gulp.watch(['/manifest.json'], ['manifest', reload]);
+});
